46134778cc34a303a165835ef506ebf02e2235d7
TabacaruAndreiC <tabacaruandrei20@gmail.com>
2023-02-10 00:04:28 +0200

    Files

---
+++ Triviador-Latest/ConsoleApplication1/Triviador.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include "Game.h"
+int main()
+{
+	Player player1("player1", 0, Player::TipPlayer::Blue);
+	Player player2("player2", 0, Player::TipPlayer::Yellow);
+	Player player3("player3", 0, Player::TipPlayer::Red);
+
+	std::vector<Player> players;
+	players.push_back(player1);
+	players.push_back(player2);
+	players.push_back(player3);
+
+	Board board((int)players.size());
+
+	QuestionsBank questionsBank("questions.txt");
+
+	Game game(players, questionsBank, board);
+	game.ChooseBasePhase();
+	std::cout << game.GetBoard();
+	game.ChooseTerritoryPhase();
+	std::cout << game.GetBoard();
+	game.DuelPhase();
+	game.ChooseWinnerPhase();
+	return 0;
+}
